7c25c49cae4177b9d9aed796d010a1f65fae9093
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-10-13 19:37:05 +0100

    Modified scenario name for visit_home_page feature test

--- spec/features/visit_home_page_spec.rb
+++ spec/features/visit_home_page_spec.rb
@@ -1,5 +1,5 @@
 feature 'Visit home page: ' do
-  scenario 'visiting the index page (home page)' do
+  scenario 'can visit the index page (home page) and see the title Bookmark Manager' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
   end
