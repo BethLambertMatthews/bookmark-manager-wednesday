e5f699a0369e051704728df45b25533ad4df4419
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-10-15 11:53:01 +0100

    Renamed previous feature tests and bookmark class with _draft. Part way through Chapter 3, viewing bookmarks

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,27 +1,2 @@
-require 'pg'
-
 class Bookmark
-
-  attr_reader :name, :url
-
-  def initialize(name, url)
-    @name = name
-    @url = url
-  end
-
-  def self.all
-    connection = PG.connect(dbname: 'bookmark_manager')
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark['url'] }
-    # result.map { |bookmark| bookmark['name'] }
-  end
-
-  def ==(other_object)
-    if other_object.class == self.class
-      return self.name == other_object.name && self.url == other_object.url
-    end
-    
-    return false  
-  end
-
 end
